80d029b0be7b74a459ca0941e231229cba8136c4
bekirbali <bfbali43@gmail.com>
2023-06-26 21:46:58 +0300

    landing page correction

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Donate = () => {
   return (
-    <div id="donate" className="h-[calc(100vh-24px)] bg-cyan-400">
+    <div id="donate" className="h-[calc(100vh-24px)]">
       Donate
     </div>
   );
