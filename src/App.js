e2cfd30766c211df1df6cabef659094b9de50c1b
bekirbali <bfbali43@gmail.com>
2023-07-06 14:56:33 +0300

    new photos new home look

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import { useRef, useState } from "react";
 import Footer from "./components/Footer";
+import GetInvolved from "./pages/GetInvolved";
+import Donate from "./pages/Donate";
 
 const App = () => {
   const scrollRef = useRef(null);
@@ -21,53 +23,28 @@ const App = () => {
     //! Navbar active colors by scrolling
     if (scrollRef.current.scrollTop === 0) {
       setActive("");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 5975) {
+    } else if (scrollRef.current.scrollTop > 14979) {
       setActive("team");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 5125) {
+    } else if (scrollRef.current.scrollTop > 14079) {
       setActive("stories");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 4225) {
+    } else if (scrollRef.current.scrollTop > 13179) {
       setActive("projects");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 3326) {
+    } else if (scrollRef.current.scrollTop > 12279) {
       setActive("involved");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 2475) {
+    } else if (scrollRef.current.scrollTop > 11379) {
       setActive("donate");
-      console.log(active);
-    } else if (scrollRef.current.scrollTop > 1575) {
+    } else if (scrollRef.current.scrollTop > 10479) {
       setActive("contact");
-      console.log(active);
     } else if (scrollRef.current.scrollTop > 696) {
       setActive("about");
-      console.log(active);
     } else if (scrollRef.current.scrollTop < 337) {
       setActive("");
-      console.log(active);
     }
   };
 
-  const handleClick = () => {
-    // if (scrollRef.current.scrollTop === 0) {
-    //   setActive("");
-    // } else if (scrollRef.current.scrollTop > 477) {
-    //   setActive("about");
-    // } else if (scrollRef.current.scrollTop > 1043) {
-    //   setActive("contact");
-    // } else if (scrollRef.current.scrollTop > 1609) {
-    //   setActive("donate");
-    // } else if (scrollRef.current.scrollTop > 2175) {
-    //   setActive("involved");
-    // } else if (scrollRef.current.scrollTop > 2741) {
-    //   setActive("projects");
-    // } else if (scrollRef.current.scrollTop > 3307) {
-    //   setActive("stories");
-    // } else if (scrollRef.current.scrollTop > 3873) {
-    //   setActive("team");
-    // }
+  const iconClick = () => {
     console.log(scrollRef.current.scrollTop);
+    scrollRef.current.scrollTo(0, 0);
   };
 
   return (
@@ -75,11 +52,14 @@ const App = () => {
       ref={scrollRef}
       onScroll={scrollHandler}
       style={{ height: "100vh", overflow: "auto" }}
-      onClick={handleClick}
     >
       <NavBar navH={navH} active={active} setActive={setActive} />
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route
+          path=""
+          element={<Home iconClick={iconClick} active={active} />}
+        />
+        <Route path="/donate" element={<Donate />} />
       </Routes>
       <Footer />
     </div>
